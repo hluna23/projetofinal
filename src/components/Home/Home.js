12021ab7979a451924fa00e1fd1f6758a317ef90
jazmin pinto <jazminpinto61@gmail.com>
2022-12-16 18:59:17 -0300

    ajustando o home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,27 @@
 import { Link } from "react-router-dom";
 import circulo from "../img/circulo.png";
 import aro from "../img/aro.png";
-import "./Home.css"
+import "./Home.css";
 export default function Home() {
   return (
     <main>
-
       <div className="fondo">
         <h1 className="titulo">Guia do Comercio</h1>
-          <div className="circuloAro">
-            <img src={circulo} alt="logo" className="Circulo"/>
-          <img src={aro} alt="logo" className="Aro"/>
-          </div>
+        <img src={circulo} alt="logo" className="Circulo" />
+        <img src={aro} alt="logo" className="Aro" />
       </div>
       <div className="Menu">
         <Link to="/cadastrar">
-            <p className="Btn" id='BtnNeg' >Anunciantes</p>
-          </Link>
-          <Link to="/produtos">
-            <p className="Btn" id='BtnC'>Site</p>
-          </Link>
-        </div>
+          <p className="Btn"  variant="primary">
+            Anunciantes
+          </p>
+        </Link>
+        <Link to="/produtos">
+          <p className="Btn"  variant="primary">
+            Site
+          </p>
+        </Link>
+      </div>
     </main>
   );
 }
